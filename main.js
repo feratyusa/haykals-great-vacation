5e1e3b81662262c5ba7d685be51dea5a92736dc8
feratyusa <prabuega55@gmail.com>
2021-12-12 12:53:14 +0700

    Updates

--- main.js
+++ main.js
@@ -6,7 +6,90 @@
  */
 
 import * as THREE from "https://cdn.skypack.dev/three@0.135.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js";
+import { PointerLockControls } from "https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/PointerLockControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.135.0/examples/jsm/loaders/GLTFLoader.js";
+
+/**
+ * Canvas and Scene
+ * 
+ */
+
+// canvas
+const canvas = document.getElementById("c");
+
+// scene and fog
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("grey");
+
+/**
+ * Maps Representation w/o Objects
+ * 
+ * xn = Col 5 + 3*n
+ * zn = Line 31 + n
+ */
+const maps = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+/**
+ * Preload Textures
+ */
+const loader_texture = new THREE.TextureLoader();
+const texture_wall0 = loader_texture.load('texture/wall0.jpg');
+const texture_roof0 = loader_texture.load('texture/wall0.jpg');
+const texture_floor0 = loader_texture.load('texture/floor0.jpg');
+texture_floor0.wrapT = THREE.RepeatWrapping;
+texture_floor0.wrapS = THREE.RepeatWrapping;
+texture_floor0.repeat.set(100, 100);
 
 /**
  * 
@@ -14,62 +97,69 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.135.0/examples/js
  * 
  */
 
+// Hakyal
+const player = {
+  positionX: 15,
+  positionY: 3,
+  positionZ: 4
+}
+
+// Museum
+const MAP_WIDTH = 50;
+const MAP_LENGTH = 50;
+const WALL_WIDTH_DEPTH = 2;
+const WALL_HEIGHT = 60;
+const ROOF_WIDTH = 100;
+const ROOF_HEIGHT = 100;
+// const WALL_COLOR = 0xadd8e6;
+
 // Make PlaneGeometry
-function makePlane() {
-  const geometry = new THREE.PlaneGeometry(200, 200);
-  const material = new THREE.MeshBasicMaterial();
+function makeFloor(texture) {
+  const geometry = new THREE.PlaneGeometry(100, 100);
+  const material = new THREE.MeshBasicMaterial({map: texture});
   material.color = new THREE.Color(0xffffff);
   material.side = THREE.DoubleSide;
-  const plane = new THREE.Mesh(geometry, material);
-  plane.position.set(0, -1, 0);
-  plane.rotation.x = Math.PI * -0.5;
-  return plane;
+  const floor = new THREE.Mesh(geometry, material);
+  floor.position.set(50, 0, 50);
+  floor.rotation.x = Math.PI * -0.5;
+  return floor;
 }
 
-// Make BoxGeometry
-function makeBox() {
-  const geometry = new THREE.BoxGeometry(2, 15, 2);
-  const material = new THREE.MeshBasicMaterial();
-  material.color = new THREE.Color(0xadd8e6);
+// Make Roof
+function makeRoof(texture){
+  const geometry = new THREE.BoxGeometry(ROOF_WIDTH, ROOF_HEIGHT, 2);
+  const material = new THREE.MeshBasicMaterial({ map: texture });
   material.side = THREE.DoubleSide;
-  const box = new THREE.Mesh(geometry, material);
-  return box;
+  const roof = new THREE.Mesh(geometry, material);
+  roof.rotation.x = Math.PI / 2;
+  roof.position.set(50, 40, 50);
+  return roof;
 }
 
-// Map
-function makeMap() {
-  let z = 0;
-  const maps = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 
-    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  ];
+// Make wall
+function makeWall(texture) {
+  const geometry = new THREE.BoxGeometry(WALL_WIDTH_DEPTH, WALL_HEIGHT, WALL_WIDTH_DEPTH);
+  const material = new THREE.MeshBasicMaterial({map: texture});
+  material.side = THREE.DoubleSide;
+  const wall = new THREE.Mesh(geometry, material);
+  return wall;
+}
 
-  for (let i = 0; i < 300; i++) {
-    if (i > 0 && i % 15 == 0) z++;
-    if (maps[i] == 1) {
-      const wall = makeBox();
-      wall.position.set((i % 15) * 2, -1, z * 2);
-      scene.add(wall);
+// Make Map
+function makeMap() {
+  for (let z = 0; z < MAP_WIDTH; z++) {
+    for(let x = 0; x < MAP_LENGTH; x++){
+        if (maps[z][x] == 1) {
+          const wall = makeWall(texture_wall0);
+          wall.position.set(x * WALL_WIDTH_DEPTH, 6, z * WALL_WIDTH_DEPTH);
+          scene.add(wall);
+        }
     }
   }
+
+  const roof = makeRoof(texture_roof0);
+  const floor = makeFloor(texture_floor0);
+  scene.add(roof, floor);
 }
 
 function resizeRendererToDisplaySize(renderer) {
@@ -83,27 +173,133 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
-
 /**
- * The builder and main function of the app
+ * Objects GLTF
  * 
  */
 
-// canvas
-const canvas = document.getElementById("c");
+// Class Object GLTF
+class gltf_object {
+  constructor(name, objPath, panoramaPath){
+    this.Name = name;
+    this.ObjectPath = objPath;
+    this.PanoramaPath = panoramaPath;
+  }
+  setPos(x, z){
+    this.pos = {
+      x: x,
+      y: 0,
+      z: z
+    }
+  }
+  setObj(obj){
+    this.Object = obj;
+  }
+  getPos(){
+    return this.pos;
+  }
+  getObj(){
+    return this.Object;
+  }
+  getObjPath(){
+    return this.ObjectPath;
+  }
+  getPanorama(){
+    return this.PanoramaPath;
+  }
+}
 
-// scene and fog
-const scene = new THREE.Scene();
-scene.background = new THREE.Color("grey");
+var objects = [];
+const gltf_loader = new GLTFLoader();
+function onload(gltf, Object, x, z, scalar, rotationY){
+  // Add Object
+  Object.setPos(x, z);
+  Object.setObj(gltf.scene);
+  const obj = Object.getObj();
+  obj.scale.multiplyScalar(scalar); // adjust scalar factor to match your scene scale
+  obj.position.x = Object.pos.x * WALL_WIDTH_DEPTH; // once rescaled, position the model where needed
+  obj.position.z = Object.pos.z * WALL_WIDTH_DEPTH;
+  if(rotationY){
+    obj.rotation.y = rotationY;
+  }
+  scene.add(obj);
+  // Add First (Top) Light
+  const light1 = new THREE.SpotLight(0xffffff, 0.5);
+  light1.position.set(obj.position.x, 60, obj.position.z);
+  light1.target.position.set(obj.position.x, 0, obj.position.z);
+  scene.add(light1);
+  scene.add(light1.target);
+  // Add Second (Front/Back) Light
+  const light2 = new THREE.DirectionalLight(0xffffff, 0.25);
+  light2.position.set(obj.position.x + 6, 0, obj.position.z);
+  light2.target.position.set(obj.position.x, 40, obj.position.z);
+  scene.add(light2);
+  scene.add(light2.target);
+  // Add Third (Front/Back) Light
+  const light3 = new THREE.DirectionalLight(0xffffff, 0.25);
+  light3.position.set(obj.position.x - 6, 0, obj.position.z);
+  light3.target.position.set(obj.position.x, 40, obj.position.z);
+  scene.add(light3);
+  scene.add(light3.target);
+}
 
+/**
+ * Object Monas
+ */
+const monas_pos = {
+  x: 3,
+  z: 15
+}
+var monas = new gltf_object('Monas', 'obj/monas/scene.gltf', '/');
+gltf_loader.load(monas.getObjPath(), function (gltf) {
+    onload(gltf, monas, monas_pos.x, monas_pos.z, 1/3);
+});
+
+/**
+ * Object Pisa
+ */
+const pisa_pos = {
+  x: 3,
+  z: 25
+}
+var pisa = new gltf_object('Pisa Tower', 'obj/pisa/scene.gltf', '/');
+gltf_loader.load(pisa.getObjPath(), function (gltf) {
+    onload(gltf, pisa, pisa_pos.x, pisa_pos.z, 1/4, Math.PI / 2);
+});
+
+/**
+ * Object Eiffel
+ */
+const eiffel_pos = {
+  x: 20,
+  z: 15
+}
+var eiffel = new gltf_object('Eiffel Tower', 'obj/eiffel/scene.gltf', '/');
+gltf_loader.load(eiffel.getObjPath(), function (gltf) {
+    onload(gltf, eiffel, eiffel_pos.x, eiffel_pos.z, 1);
+});
+
+/** 
+ * Object Taj Mahal
+ */
+const tajmahal_pos = {
+  x: 12,
+  z: 40
+}
+var tajmahal = new gltf_object('Taj Mahal', 'obj/tajmahal/scene.gltf', '/');
+gltf_loader.load(tajmahal.getObjPath(), function (gltf) {
+    onload(gltf, tajmahal, tajmahal_pos.x, tajmahal_pos.z, 1/10);
+});
+
+objects.push(monas, pisa, eiffel, tajmahal);
+
+/**
+ * Camera and Controls
+ * 
+ */
 // camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(0, 0, 20);
-
-// Light
-const lightB = new THREE.DirectionalLight(0xffffff, 1);
-lightB.position.set(0, 0, 40);
-scene.add(lightB);
+camera.position.set(player.positionX, player.positionY, player.positionZ);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
@@ -111,16 +307,43 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 
 // Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window);
-controls.enableDampling = true;
-controls.damplingFactor = 0.5;
-controls.enableZoom = false;
-controls.update();
+const controls = new PointerLockControls(camera, renderer.domElement);
 
-// Objects
-const plane = makePlane();
-scene.add(plane);
+document.addEventListener("click", function () {
+  controls.lock();
+});
+
+scene.add(controls.getObject());
+
+// Movements
+let speed = 1 / 2;
+
+const onKeyDown = function (e) {
+  switch (e.code) {
+    case "KeyW":
+      controls.moveForward(speed);
+      break;
+    case "KeyA":
+      controls.moveRight(-speed);
+      break;
+    case "KeyD":
+      controls.moveRight(speed);
+      break;
+    case "KeyS":
+      controls.moveForward(-speed);
+      break;
+    default:
+      break;
+  }
+};
+document.addEventListener("keydown", onKeyDown, false);
+
+
+/**
+ * Main app
+ * 
+ */
+
 makeMap();
 
 const mainloop = function () {
@@ -131,7 +354,7 @@ const mainloop = function () {
   }
 
   renderer.render(scene, camera);
-  controls.update();
+  // controls.update();
   requestAnimationFrame(mainloop);
 };
 mainloop();
