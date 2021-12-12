28b6c117ace2a01c9215b5f8d8689069ae30b885
VickyThirdian <54606856+thirdianv@users.noreply.github.com>
2021-12-12 19:22:15 +0700

    add some objects

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,8 @@
 /**
  * For testing you can use ../node_modules/three/
- * 
+ *
  * For Deployment use CDN from skypack.dev
- * 
+ *
  */
 
 import * as THREE from "https://cdn.skypack.dev/three@0.135.0";
@@ -19,7 +19,7 @@ var state = PLAY;
 
 /**
  * Canvas and Scene
- * 
+ *
  */
 
 // canvas
@@ -31,7 +31,7 @@ scene.background = new THREE.Color("grey");
 
 /**
  * Maps Representation w/o Objects
- * 
+ *
  * xn = Col 5 + 3*n
  * zn = Line 31 + n
  */
@@ -69,7 +69,7 @@ const maps = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -86,7 +86,7 @@ const maps = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  ];
+];
 
 /**
  * Sphere for Panorama View
@@ -97,26 +97,28 @@ var panoramaSphere;
  * Preload Textures
  */
 const loader_texture = new THREE.TextureLoader();
-const texture_wall0 = loader_texture.load('texture/wall0.jpg');
-const texture_roof0 = loader_texture.load('texture/wall0.jpg');
-const texture_floor0 = loader_texture.load('texture/floor0.jpg');
+const texture_wall0 = loader_texture.load("texture/wall0.jpg");
+const texture_roof0 = loader_texture.load("texture/wall0.jpg");
+const texture_floor0 = loader_texture.load("texture/floor0.jpg");
 texture_floor0.wrapT = THREE.RepeatWrapping;
 texture_floor0.wrapS = THREE.RepeatWrapping;
 texture_floor0.repeat.set(100, 100);
-const pan_monas = loader_texture.load('panorama/monas.png');
+const pan_monas = loader_texture.load("panorama/monas.png");
+const pan_pisa = loader_texture.load("panorama/pisa.png");
+const pan_eiffel = loader_texture.load("panorama/ColesseumPanoramic.jpg");
 
 /**
- * 
+ *
  * All the functions and utils for the app
- * 
+ *
  */
 
 // Hakyal
 const player = {
   positionX: 15,
   positionY: 3,
-  positionZ: 4
-}
+  positionZ: 4,
+};
 
 // Museum
 const MAP_WIDTH = 50;
@@ -129,7 +131,7 @@ const ROOF_HEIGHT = 100;
 // Make PlaneGeometry
 function makeFloor(texture) {
   const geometry = new THREE.PlaneGeometry(100, 100);
-  const material = new THREE.MeshBasicMaterial({map: texture});
+  const material = new THREE.MeshBasicMaterial({ map: texture });
   material.color = new THREE.Color(0xffffff);
   material.side = THREE.DoubleSide;
   const floor = new THREE.Mesh(geometry, material);
@@ -139,7 +141,7 @@ function makeFloor(texture) {
 }
 
 // Make Roof
-function makeRoof(texture){
+function makeRoof(texture) {
   const geometry = new THREE.BoxGeometry(ROOF_WIDTH, ROOF_HEIGHT, 2);
   const material = new THREE.MeshBasicMaterial({ map: texture });
   material.side = THREE.DoubleSide;
@@ -152,18 +154,18 @@ function makeRoof(texture){
 // Make wall
 function makeWall(texture) {
   const geometry = new THREE.BoxGeometry(WALL_WIDTH_DEPTH, WALL_HEIGHT, WALL_WIDTH_DEPTH);
-  const material = new THREE.MeshBasicMaterial({map: texture});
+  const material = new THREE.MeshBasicMaterial({ map: texture });
   material.side = THREE.DoubleSide;
   const wall = new THREE.Mesh(geometry, material);
   return wall;
 }
 
 // Sphere for Panorama
-function makePanoramaSphere(){
+function makePanoramaSphere() {
   const geometry = new THREE.SphereGeometry(500, 100, 80);
   const material = new THREE.MeshBasicMaterial({
     side: THREE.BackSide,
-    map: pan_monas
+    map: pan_monas,
   });
   const sphere = new THREE.Mesh(geometry, material);
   sphere.position.set(0, -1000, 0);
@@ -173,23 +175,24 @@ function makePanoramaSphere(){
 // Make Map
 function makeMap() {
   for (let z = 0; z < MAP_WIDTH; z++) {
-    for(let x = 0; x < MAP_LENGTH; x++){
-        if (maps[z][x] == 1) {
-          const wall = makeWall(texture_wall0);
-          wall.position.set(x * WALL_WIDTH_DEPTH, 6, z * WALL_WIDTH_DEPTH);
-          scene.add(wall);
-        }
+    for (let x = 0; x < MAP_LENGTH; x++) {
+      if (maps[z][x] == 1) {
+        const wall = makeWall(texture_wall0);
+        wall.position.set(x * WALL_WIDTH_DEPTH, 6, z * WALL_WIDTH_DEPTH);
+        scene.add(wall);
+      }
     }
   }
 
   const roof = makeRoof(texture_roof0);
   const floor = makeFloor(texture_floor0);
   panoramaSphere = makePanoramaSphere();
+
   scene.add(roof, floor, panoramaSphere);
 }
 
 // Set Panorama Image
-function setPanoramaImage(panorama){
+function setPanoramaImage(panorama) {
   panoramaSphere.material.map = image;
 }
 
@@ -206,49 +209,49 @@ function resizeRendererToDisplaySize(renderer) {
 
 /**
  * Objects GLTF
- * 
+ *
  */
 
 // Class Object GLTF
 class gltf_object {
-  constructor(name, objPath, panorama){
+  constructor(name, objPath, panorama) {
     this.Name = name;
     this.ObjectPath = objPath;
     this.Panorama = panorama;
   }
-  setPos(x, z){
+  setPos(x, z) {
     this.pos = {
       x: x,
       y: 0,
-      z: z
-    }
+      z: z,
+    };
   }
-  setObj(obj){
+  setObj(obj) {
     this.Object = obj;
   }
-  setMesh(mesh){
+  setMesh(mesh) {
     this.mesh = mesh;
   }
-  getPos(){
+  getPos() {
     return this.pos;
   }
-  getObj(){
+  getObj() {
     return this.Object;
   }
-  getMesh(){
+  getMesh() {
     return this.mesh;
   }
-  getObjPath(){
+  getObjPath() {
     return this.ObjectPath;
   }
-  getPanorama(){
+  getPanorama() {
     return this.Panorama;
   }
 }
 
 var objects = [];
 const gltf_loader = new GLTFLoader();
-function onload(gltf, Object, x, z, scalar, rotationY){
+function onload(gltf, Object, x, z, scalar, rotationY) {
   // Add Object
   Object.setPos(x, z);
   Object.setObj(gltf.scene);
@@ -256,19 +259,26 @@ function onload(gltf, Object, x, z, scalar, rotationY){
   obj.scale.multiplyScalar(scalar); // adjust scalar factor to match your scene scale
   obj.position.x = Object.pos.x * WALL_WIDTH_DEPTH; // once rescaled, position the model where needed
   obj.position.z = Object.pos.z * WALL_WIDTH_DEPTH;
-  if(rotationY){
+  if (rotationY) {
     obj.rotation.y = rotationY;
   }
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(10, 10, 10), 
-    new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0 })
-  );
+
+  const mesh = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0 }));
   mesh.position.x = obj.position.x;
   mesh.position.y = 5;
   mesh.position.z = obj.position.z;
   Object.setMesh(mesh);
   scene.add(mesh);
   scene.add(obj);
+
+  const mesh1 = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0 }));
+  mesh1.position.x = obj.position.x;
+  mesh1.position.y = 5;
+  mesh1.position.z = obj.position.z;
+  Object.setMesh(mesh1);
+  scene.add(mesh1);
+  scene.add(obj);
+
   // Add First (Top) Light
   const light1 = new THREE.SpotLight(0xffffff, 0.5);
   light1.position.set(obj.position.x, 60, obj.position.z);
@@ -294,24 +304,48 @@ function onload(gltf, Object, x, z, scalar, rotationY){
  */
 const monas_pos = {
   x: 3,
-  z: 15
-}
-var monas = new gltf_object('Monas', 'obj/monas/scene.gltf', pan_monas);
+  z: 15,
+};
+var monas = new gltf_object("Monas", "obj/monas/scene.gltf", pan_monas);
 gltf_loader.load(monas.getObjPath(), function (gltf) {
-    onload(gltf, monas, monas_pos.x, monas_pos.z, 1/3);
+  onload(gltf, monas, monas_pos.x, monas_pos.z, 1 / 2);
 });
 
 /**
  * Object Pisa
  */
-// const pisa_pos = {
-//   x: 3,
-//   z: 25
-// }
-// var pisa = new gltf_object('Pisa Tower', 'obj/pisa/scene.gltf', '/');
-// gltf_loader.load(pisa.getObjPath(), function (gltf) {
-//     onload(gltf, pisa, pisa_pos.x, pisa_pos.z, 1/4, Math.PI / 2);
-// });
+const pisa_pos = {
+  x: 30,
+  z: 15,
+};
+var pisa = new gltf_object("Pisa Tower", "obj/pisa/scene.gltf", pan_pisa);
+gltf_loader.load(pisa.getObjPath(), function (gltf) {
+  onload(gltf, pisa, pisa_pos.x, pisa_pos.z, 1 / 4, Math.PI / 2);
+});
+
+const eiffel_pos = {
+  x: 20,
+  z: 15,
+};
+var eiffel = new gltf_object("Eiffel", "obj/eiffel/scene.gltf", pan_eiffel);
+gltf_loader.load(eiffel.getObjPath(), function (gltf) {
+  onload(gltf, eiffel, eiffel_pos.x, eiffel_pos.z, 1.5, Math.PI / 2);
+});
+
+//coba lukisan
+
+// const boro_pos = {
+//   x: 20,
+//   z: 15,
+// };
+
+// const borobudur = new THREE.TextureLoader().load("borobudur.jpg");
+
+// const gambar = new THREE.Mesh(new THREE.PlaneGeometry(1.6, 2), new THREE.MeshPhongMaterial({ map: borobudur, side: THREE.DoubleSide }));
+// scene.add(gambar);
+// gambar.boro_pos(-2.5).translateY(1.26).translateZ(-3.85);
+// gambar.rotation.y = Math.PI / 2;
+// gambar.rotateOnAxis(new THREE.Vector3(0, -1, 0), Math.PI / 2);
 
 // /**
 //  * Object Eiffel
@@ -325,7 +359,7 @@ gltf_loader.load(monas.getObjPath(), function (gltf) {
 //     onload(gltf, eiffel, eiffel_pos.x, eiffel_pos.z, 1);
 // });
 
-// /** 
+// /**
 //  * Object Taj Mahal
 //  */
 // const tajmahal_pos = {
@@ -338,10 +372,12 @@ gltf_loader.load(monas.getObjPath(), function (gltf) {
 // });
 
 objects.push(monas);
+objects.push(pisa);
+objects.push(eiffel);
 
 /**
  * Camera, Renderer, Controls, and Raycaster
- * 
+ *
  */
 // camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
@@ -359,43 +395,40 @@ const mouse = new THREE.Vector2();
 // Controls
 const controls = new PointerLockControls(camera, renderer.domElement);
 
-const onMouseClick = function(e){
-  if(controls.isLocked == false){
+const onMouseClick = function (e) {
+  if (controls.isLocked == false) {
     controls.lock();
-  }
-  else{
+  } else {
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
 
     raycast.setFromCamera(mouse, camera);
     const intersects = raycast.intersectObjects(scene.children, true);
 
-    if(intersects.length > 0){
+    if (intersects.length > 0) {
       var showPanorama = false;
       var object;
-      for(var i=0; i < objects.length; i++){
+      for (var i = 0; i < objects.length; i++) {
         const obj = objects[i].getMesh();
-        if(intersects[0].object == obj){
+        if (intersects[0].object == obj) {
           showPanorama = true;
           object = objects[i];
           break;
         }
       }
-      if(showPanorama){
+      if (showPanorama) {
         document.getElementById("mouse").innerHTML = "Nice";
         player.positionX = camera.position.x;
         player.positionY = camera.position.y;
         player.positionZ = camera.position.z;
         // setPanoramaImage(object.getPanorama());
         camera.position.set(0, -1000, 0);
-      }
-      else{
+      } else {
         document.getElementById("mouse").innerHTML = "Not Nice";
       }
     }
-
   }
-}
+};
 
 document.addEventListener("click", onMouseClick, false);
 
@@ -422,10 +455,9 @@ const onKeyDown = function (e) {
 };
 document.addEventListener("keydown", onKeyDown, false);
 
-
 /**
  * Main app
- * 
+ *
  */
 
 makeMap();
@@ -438,10 +470,10 @@ const mainloop = function () {
   }
 
   renderer.render(scene, camera);
-  for(var i=0; i < objects.length; i++){
-    if (objects[0].getObj()) {
-      const obj = objects[0].getObj();
-      obj.rotation.y += 0.1;
+  for (var i = 0; i < objects.length; i++) {
+    if (objects[i].getObj()) {
+      const obj = objects[i].getObj();
+      obj.rotation.y += 0.01;
     }
   }
   requestAnimationFrame(mainloop);
